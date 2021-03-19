5d3040de0f296f6bc355cbe3ea578cadac4ac04e
SanjnaKumari <sanjna.kumari325@gmail.com>
2021-03-19 15:40:59 +0500

    About us page updated

--- src/Data/Home_Data.jsx
+++ src/Data/Home_Data.jsx
@@ -105,4 +105,31 @@ const Aboutdetail_data = [
         title2: "Vision" 
      },
 ]
-export {Sdata, Heading_data, C_data, Aboutqualities_data, Aboutdetail_data}
+
+const Headerdata =[
+    {heading:"Services",l1:"Services" },
+    // {heading:"Services Detail",l1:"Services", l2:"Service Details"},
+    {heading: "About Us",l1: "About Us" },
+    {heading: "Contact Us", l1: "Contact Us" },
+    {heading: "Portfolio 2 Column", l1: "Portfolio 2 Column"},
+    {heading: "Portfolio 3 Column", l1: "Portfolio 3 Column"},
+    {heading: "Blog 2 Columns", l1:"Blog 2 Columns"},
+    {heading: "Blog 3 Columns", l1:"Blog 3 Columns"},
+    {heading:"Blog Right Sidebar", l1:"Blog 3 Columns"},
+    {heading:"Blog Left Sidebar", l1:"Blog 3 Columns"}
+    // {heading: "Portfolio Details",liClass:"breadcrumb-item active"}
+]
+
+const Headerdata_left=[
+    {heading: "About Us", l1:"About Us"},
+    {heading: "Services", l1:"Services"},
+    {heading: "Blog 3 Column", l1:"Blog 3 Column"}
+]
+
+const Headerdata_detail=[
+    {heading:"Blog Detail", l1:"Blog"},
+    {heading:"Service Detail", l1:"Services"},
+    {heading:"Portfolio Detail", l1:"Portfolio"}
+]
+
+export {Sdata, Heading_data, C_data, Aboutqualities_data, Aboutdetail_data, Headerdata, Headerdata_left, Headerdata_detail}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import Aboutbanner from '../sections/Aboutbanner'
+import Headerleft from '../components/Headers/Headerleft'
 import About_content from '../components/About_content'
 import Aboutqualities from '../sections/Aboutqualities'
 import Aboutdetail from '../sections/Aboutdetail'
 import Counter from '../sections/Counter'
 import Ourteam from '../sections/Ourteam'
+import { Headerdata_left } from '../Data/Home_Data'
 
 const AboutUs = () =>{
     return(
         <>
         <div className="wraper-v2-inner">
-        <Aboutbanner/>
+        <Headerleft
+            heading = {Headerdata_left[0].heading}
+            l1 = {Headerdata_left[0].l1}
+        />
         <About_content />
         <Aboutqualities />
         <Aboutdetail />
